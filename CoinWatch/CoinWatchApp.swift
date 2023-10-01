ea36a565c311c2f0628004c85a9ec644c2014f49
Michael Lyapin <lyapin.dev@gmail.com>
2023-10-01 14:44:40 +0300

    Initial Commit

---
+++ CoinWatch/CoinWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinWatchApp.swift
+//  CoinWatch
+//
+//  Created by Ляпин Михаил on 01.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoinWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
